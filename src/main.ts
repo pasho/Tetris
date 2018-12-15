b7d6ab1db3712263ca95c8d113f6a286818672f5
pavel.fedulov <pfedulov84@gmail.com>
2018-12-15 18:06:55 +0000

    basic playfield

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+window.onload = () => {
+    let playField = document.getElementById('playField') as HTMLCanvasElement
+
+    playField.width = window.innerWidth
+    playField.height = window.innerHeight
+
+    let blockSize = 25
+    let playFieldSize = { height: 22, width: 10 }
+
+    let ctx = playField.getContext('2d')
+    ctx.moveTo(0, 0)
+    ctx.lineTo(0, playFieldSize.height * blockSize + 1)
+    ctx.lineTo(playFieldSize.width * blockSize + 1, playFieldSize.height * blockSize + 1)
+    ctx.lineTo(playFieldSize.width * blockSize + 1, 0)
+    ctx.stroke()
+}
